9aad3b6832ce6c1e14aadb490ff46a1b32764644
Ron West <ron@inquisibee.com>
2018-09-12 09:39:38 -0400

    * Changed routing for JSONOddsController

--- php/src/AppBundle/Controller/JSONOddsController.php
+++ php/src/AppBundle/Controller/JSONOddsController.php
@@ -5,7 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class JSONOddsController extends Controller
 {
